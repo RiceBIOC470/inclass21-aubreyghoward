61622c944abd1cecb8c8c167311f5e63805b89ed
aubreyghoward <aubrey.g.howard@rice.edu>
2017-11-16 14:31:39 -0600

    Nearly done

---
+++ Q1func.m
@@ -0,0 +1,8 @@
+function [dx] = Q1func (t,StartArray)
+dx = zeros(2,1);
+% Lek k,k2 = 2. d,d2 = 4;
+x = StartArray(1);
+y = StartArray(2);
+dx(1) = 2-4.*x; %k - d*x
+dx(2) = 2.*x-4.*y;  %k2*x - d2*y
+end
---
+++ Q2func.m
@@ -0,0 +1,9 @@
+% dx/dt = y
+% dy/dt = (1-x^2)*y - x
+
+function dx = Q2func(t,x);
+
+dx = zeros(2,1);
+dx(1) = x(1);
+dx(2) = (1-x(1).^2)*x(2)-x(1);
+end
